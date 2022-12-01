203a1ebfd99f40ec1d9e000fb3ae92d9c9508aa1
Florian Warzecha <liketechnik@disroot.org>
2022-12-01 19:43:00 +0100

    build: flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "dependencies for my AoC 2022 solutions";
+
+  inputs.nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in rec {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = (with pkgs; [
+
+            racket
+
+          ]);
+        };
+      }
+    );
+}
